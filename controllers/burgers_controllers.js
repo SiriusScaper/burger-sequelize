35e668cd3b97d526fdf7d2fad07f2322aefb2fc5
SiriusScaper <siriusscaper@gmail.com>
2018-05-10 03:25:52 -0700

    Attempting to get sequelize working

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -2,32 +2,34 @@ const express = require('express');
 const router = express.Router();
 const Burgers = require('../models/burger');
 
-app.get('/api/all', (req, res) => {
-	Burgers.findAll().then(res);
-});
-
-// router.get('/', (req, res) => {
-// 	burger.selectAll(burger => {
-// 		console.log(burger);
-// 		res.render('index', { burgers: burger });
-// 	});
-// });
-
-app.post('/api/burgers', (req, res) => {});
-
-router.post('/api/burgers', (req, res) => {
-	console.log(req.body);
-	burger.insertOne(req.body.burger_name, res => {
-		res.redirect('/');
+module.exports = function(app) {
+	app.get('/api/all', (req, res) => {
+		Burgers.selectAll(Burgers).then(res =>
+			res.render('index', { burgers: burger })
+		);
 	});
-});
 
-router.put('/api/burgers/:id', (req, res) => {
-	let condition = `id = ${req.params.id}`;
-	burger.updateOne({ devoured: true }, condition, res => {
-		console.log(res.body);
-		res.redirect('/');
+	// router.get('/', (req, res) => {
+	// 	burger.selectAll(burger => {
+	// 		console.log(burger);
+	// 		res.render('index', { burgers: burger });
+	// 	});
+	// });
+
+	// app.post('/api/burgers', (req, res) => {});
+
+	router.post('/api/burgers', (req, res) => {
+		console.log(req.body);
+		burger.insertOne(req.body.burger_name, res => {
+			res.redirect('/');
+		});
 	});
-});
 
-module.exports = router;
+	router.put('/api/burgers/:id', (req, res) => {
+		let condition = `id = ${req.params.id}`;
+		burger.updateOne({ devoured: true }, condition, res => {
+			console.log(res.body);
+			res.redirect('/');
+		});
+	});
+};
